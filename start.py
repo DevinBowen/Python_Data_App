0ce2705e29d56340e9cc705d4d5dafdb1d46ba2f
devin <bowen61496@gmail.com>
2023-06-23 13:36:01 -0400

    stuff

--- start.py
+++ start.py
@@ -1,9 +1,13 @@
 import tkinter as tk
-import pandas
+#import pandas
 
 
 window = tk.Tk()
 window.title = 'Main Window'
 # toplevel = tk.Toplevel(window)
 # toplevel.title = 'Top Level'
+message = tk.Label(window, text="Hello, World!")
+message.pack()
 window.mainloop()
+
+print("Hello World") 
