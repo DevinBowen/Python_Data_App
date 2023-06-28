cdb42bd38dbdf8b97e0bb6c4bece58727027ed2a
devin <bowen61496@gmail.com>
2023-06-28 00:07:42 -0400

    pandas

--- start.py
+++ start.py
@@ -1,6 +1,24 @@
 import tkinter as tk
-#import pandas
+import pandas as pd
+import matplotlib as plt
+import csv
 
+df = pd.read_csv('Dataset/20200118/310/summary.csv')
+
+print(df.head(10))
+
+x = []
+y = []
+
+with open('Dataset/20200118/310/summary.csv','r') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',')
+    for row in lines:
+        x.append(row[0])
+        y.append(int(row[2]))
+
+plt.plot(x, y, color = 'g', width = 0.72, label = "data")
+plt.legend()
+plt.show()
 
 window = tk.Tk()
 window.title = 'Main Window'
@@ -11,5 +29,7 @@ message.pack()
 window.mainloop()
 
 print("Hello World") 
+print(pd.__version__)
+print(plt.__version__)
 
 ###test###
