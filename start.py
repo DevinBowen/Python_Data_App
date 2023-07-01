008819d939cbcff65b08230ed3ed1a5b9a98a24f
devin <bowen61496@gmail.com>
2023-07-01 09:38:09 -0400

    Graph working in tkinter window.

--- start.py
+++ start.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
 import csv
 
 df = pd.read_csv('Dataset/20200118/310/summary.csv')
@@ -14,22 +17,58 @@ with open('Dataset/20200118/310/summary.csv','r') as csvfile:
     lines = csv.reader(csvfile, delimiter=',')
     for row in lines:
         x.append(row[0])
-        y.append(int(row[2]))
+        y.append((row[6]))
 
-plt.plot(x, y, color = 'g', width = 0.72, label = "data")
+plt.plot(x, y, color = 'g', label = "data")
 plt.legend()
-plt.show()
+# plt.show()
+
+
+def plot():
+    # the figure that will contain the plot
+    fig = Figure(figsize = (5, 5),
+                 dpi = 100)
+  
+    # list of squares
+    y = [i**2 for i in range(101)]
+  
+    # adding the subplot
+    plot1 = fig.add_subplot(111)
+  
+    # plotting the graph
+    plot1.plot(y)
+  
+    # creating the Tkinter canvas
+    # containing the Matplotlib figure
+    canvas = FigureCanvasTkAgg(fig,
+                               master = window)  
+    canvas.draw()
+  
+    # placing the canvas on the Tkinter window
+    canvas.get_tk_widget().pack()
+  
+    # creating the Matplotlib toolbar
+    toolbar = NavigationToolbar2Tk(canvas,
+                                   window)
+    toolbar.update()
+  
+    # placing the toolbar on the Tkinter window
+    canvas.get_tk_widget().pack()
+
 
 window = tk.Tk()
-window.title = 'Main Window'
+window.title = 'Charts'
+window.geometry('500x500')
 # toplevel = tk.Toplevel(window)
 # toplevel.title = 'Top Level'
-message = tk.Label(window, text="Hello, World!")
-message.pack()
+plot_button = tk.Button(master = window, command = plot, height = 2, width = 10, text = 'plot')
+plot_button.pack()
+
+
 window.mainloop()
 
 print("Hello World") 
 print(pd.__version__)
-print(plt.__version__)
+# print(plt.__version__)
 
 ###test###
