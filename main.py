ad734a91e84870b0c9dfc9bec504c03644cc616a
devin <bowen61496@gmail.com>
2023-07-11 11:36:02 -0400

    testing boxes

--- main.py
+++ main.py
@@ -9,19 +9,38 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
 NavigationToolbar2Tk)
 import csv
+import numpy as np
+import torch
 
-df = pd.read_csv('Dataset/20200118/310/summary.csv')
+df = pd.read_csv('Dataset/20200118/310/summary.csv', nrows=2)
 
 print(df.head(10))
 
 x = []
 y = []
+Date = []
+Acc = []
+Eda = []
+Temp = []
+Mvmt = []
+Step = []
+Rest = []
+On = []
+
 
 with open('Dataset/20200118/310/summary.csv', 'r') as csvfile:
     lines = csv.reader(csvfile, delimiter=',')
     for row in lines:
         x.append(row[0])
-        y.append((row[6]))
+        y.append(row[6])
+        Date.append(row[0])
+        Acc.append(row[3])
+        Eda.append(row[4])
+        Temp.append(row[5])
+        Mvmt.append(row[6])
+        Step.append(row[7])
+        Rest.append(row[8])
+        On.append(row[9])
 
 
 def select_file():
@@ -46,7 +65,7 @@ class Graphy(tk.Tk):
         self.title("Graphy")
         self.frames = dict()
 
-        container = ttk.Frame(self)
+        container = LabelFrame(self, bg='blue', bd=2)
         container.grid(padx=60, pady=30, sticky="EW")
 
         for FrameClass in (Graph, Chart):
@@ -65,20 +84,19 @@ class Graph(ttk.Frame):
     def __init__(self, container, controller):
         super().__init__(container)
 
-        frame = LabelFrame(self, padx=50, pady=50)
-        frame.grid(padx=10, pady=10)
+        frame = LabelFrame(self, padx=50, pady=50, bg="light blue")
+        frame.grid(padx=10, pady=10, sticky="NSEW")
 
         def plot():
-            fig = Figure(figsize = (5, 5), dpi = 100)
-            # adding the subplot
+            fig = Figure(figsize=(5, 5), dpi=100)
+            # adding the subplots
             plot1 = fig.add_subplot(111)
             # plotting the graph
-            plot1.plot(x,y)
-  
+            plot1.plot(x, y)
             # creating the Tkinter canvas
             # containing the Matplotlib figure
-            canvas = FigureCanvasTkAgg(fig, frame)  
-            canvas.draw()
+            canvas = FigureCanvasTkAgg(fig, frame)
+            # canvas.draw()
             canvas.get_tk_widget().grid(row=1, sticky="EW")
 
             # !!!Not Working!!!
@@ -87,7 +105,7 @@ class Graph(ttk.Frame):
             # toolbar.update()
             # canvas.get_tk_widget().pack()
 
-        plot_button = Button(frame, text="Graph", command = plot, height = 2, width = 10)
+        plot_button = Button(frame, text="Graph", command=plot, height=2, width=10)
         plot_button.grid(row=0, column=0)
         switch_page_button = Button(
             self,
@@ -102,7 +120,7 @@ class Chart(ttk.Frame):
         super().__init__(container)
 
         frame = LabelFrame(self, padx=50, pady=50)
-        frame.grid(padx=10, pady=10)
+        frame.grid(padx=10, pady=10, sticky="NSEW")
 
         b = Button(frame, text="Chart")
         b.grid(row=0, column=0)
@@ -115,4 +133,5 @@ class Chart(ttk.Frame):
 
 
 root = Graphy()
+root.geometry('800x800')
 root.mainloop()
