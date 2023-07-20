728e82f59d2ad83148819db98b20a3672d38e42c
devin <bowen61496@gmail.com>
2023-07-20 06:38:21 -0400

    idk

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.backend_tools import ToolBase, ToolToggleBase
 import csv
 import numpy as np
-import torch
+#import torch
 import matplotlib.ticker as plticker
 
 df = pd.read_csv('Dataset/20200118/310/summary.csv', nrows=2)
@@ -72,16 +72,32 @@ class Graphy(tk.Tk):
         container = LabelFrame(self, bg='blue', bd=2)
         container.grid(padx=60, pady=30, sticky="EW")
 
-        for FrameClass in (Graph, Chart):
+        for FrameClass in (Front, Graph, Chart):
             frame = FrameClass(container, self)
             self.frames[FrameClass] = frame
             frame.grid(row=0, column=0, sticky="NSEW")
 
-        self.show_frame(Graph)
+        self.show_frame(Front)
 
     def show_frame(self, container):
         frame = self.frames[container]
         frame.tkraise()
+
+
+class Front(ttk.Frame):
+    def __init__(self, container, controller):
+        super().__init__(container)
+
+        frame = Frame(self, padx=50, pady=50, bg="light blue")
+        frame.configure(height=500)
+        frame.grid(padx=10, pady=10, sticky="NSEW")
+
+        switch_page_button = Button(
+            frame,
+            text="Switch to Chart",
+            command=lambda: controller.show_frame(Graph)
+        )
+        switch_page_button.grid(column=0, row=2, columnspan=2, sticky="EW")
 
 
 class Graph(ttk.Frame):
@@ -137,10 +153,11 @@ class Chart(ttk.Frame):
         frame.grid(padx=10, pady=10, sticky="NSEW")
 
         def chart():
-            plt.plot(Date, Mvmt)
-            plt.show()
+            test = StringVar(Mvmt)
+            lab = Label(frame, textvariable=test)
+            lab.grid(row=1, columnspan=3, sticky="EW")
 
-        b = Button(frame, text="Chart")
+        b = Button(frame, command=chart, text="Chart")
         b.grid(row=0, column=0)
         switch_page_button = Button(
             self,
@@ -151,5 +168,4 @@ class Chart(ttk.Frame):
 
 
 root = Graphy()
-# root.geometry('800x800')
 root.mainloop()
